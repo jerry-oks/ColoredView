f3ca8358d8cf150dc2d51d2cb8f64937a23bc352
Vadim Vanatov <79990006611@yandex.ru>
2023-07-28 01:11:51 +0300

    logic coded

--- ColoredView/ViewController.swift
+++ ColoredView/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redComponentLabel: UILabel!
+    @IBOutlet var greenComponentLabel: UILabel!
+    @IBOutlet var blueComponentLabel: UILabel!
+    
+    @IBOutlet var redComponentSlider: UISlider!
+    @IBOutlet var greenComponentSlider: UISlider!
+    @IBOutlet var blueComponentSlider: UISlider!
+    
+    @IBOutlet var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 12
+        magicColorButtonTapped()
     }
 
+    private func valueChanged(of slider: UISlider, _ label: UILabel) {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redComponentSlider.value),
+            green: CGFloat(greenComponentSlider.value),
+            blue: CGFloat(blueComponentSlider.value),
+            alpha: 1
+        )
+        label.text = String(format: "%.2f", slider.value)
+    }
+    
+    @IBAction func redSliderValueChanged() {
+        valueChanged(of: redComponentSlider, redComponentLabel)
+    }
+    @IBAction func greenSliderValueChanged() {
+        valueChanged(of: greenComponentSlider, greenComponentLabel)
+    }
+    @IBAction func blueSliderValueChanged() {
+        valueChanged(of: blueComponentSlider, blueComponentLabel)
+    }
+    
+    @IBAction func magicColorButtonTapped() {
+        redComponentSlider.value = Float.random(in: 0...1)
+        greenComponentSlider.value = Float.random(in: 0...1)
+        blueComponentSlider.value = Float.random(in: 0...1)
 
+        redSliderValueChanged()
+        greenSliderValueChanged()
+        blueSliderValueChanged()
+    }
+    
 }
 
